1aac517f68d0b002b288c77f4360c0f12e236973
nad-garraz <22796744+nad-garraz@users.noreply.github.com>
2023-09-11 17:09:59 -0300

    Polishing

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='dib tc w5 h-100 grow bg-light-green br3 pa3 ma2 bw2 shadow-5'>
+    <div className='dib tc w5 h-200 grow bg-light-green br3 pa3 ma2 bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
